0a2a802dda59a9bfc622476c0f638c9b6de50584
splendidbrill <splendidbrill@gmail.com>
2026-04-30 22:24:03 +0530

    sitemap updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/dashboard", "/onboarding", "/playbook/"],
     },
-    sitemap: "https://getfarcast.com/sitemap.xml",
+    sitemap: "https://www.getfarcast.com/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://getfarcast.com";
+  const baseUrl = "https://www.getfarcast.com";
 
   return [
     {
